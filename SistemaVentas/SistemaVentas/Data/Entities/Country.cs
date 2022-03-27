97b813644b7d666107ff23ce796075b5c429d559
isaac calvo ochoa <isaaccalvoochoa@gmail.com>
2022-03-27 00:33:28 -0700

    Finish Country CRUD

---
+++ SistemaVentas/SistemaVentas/Data/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaVentas.Data.Entities
+{
+    public class Country
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "País")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required (ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; }
+    }
+}
